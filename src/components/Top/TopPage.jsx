0c2c6f3f5d532ddb812fd67ae72d9561af4eb378
saitohiroki <hirokisaito0813@gmail.com>
2022-05-28 13:33:03 +0900

    toppageの画面作成

---
+++ src/components/Top/TopPage.jsx
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from 'react';
+import styles from "../css/Top.css";
+
+const TopPage = () => {
+  return (
+    <div className='content-wrapper'>
+      <div className="top-inner">
+        <div className="sideber">
+          <h2 className="sideber-title">ここはサイドバーです。</h2>
+        </div>
+        <div className="main-content">
+          <h2 className="main-title">メインコンテンツ</h2>
+          <div className="agenda-wrapper">
+
+            <div className="agenda-wrap">
+              <div className="agenda-left">
+                <div className="agenda-title">
+                  <p>sample agenda</p>
+                </div>
+                <div className="agenda-totalling">
+                  <p>賛成:1 反対:2 計:3</p>
+                </div>
+              </div>
+              <div className="agenda-post">
+                <p className="agenda-text">
+                  sample text
+                </p>
+                <p className="text-time">
+                  00/00/00:0
+                </p>
+              </div>
+            </div>
+
+            <div className="agenda-wrap">
+              <div className="agenda-left">
+                <div className="agenda-title">
+                  <p>sample agenda</p>
+                </div>
+                <div className="agenda-totalling">
+                  <p>賛成:1 反対:2 計:3</p>
+                </div>
+              </div>
+              <div className="agenda-post">
+                <p className="agenda-text">
+                  sample text
+                </p>
+                <p className="text-time">
+                  00/00/00:0
+                </p>
+              </div>
+            </div>
+
+            <div className="agenda-wrap">
+              <div className="agenda-left">
+                <div className="agenda-title">
+                  <p>sample agenda</p>
+                </div>
+                <div className="agenda-totalling">
+                  <p>賛成:1 反対:2 計:3</p>
+                </div>
+              </div>
+              <div className="agenda-post">
+                <p className="agenda-text">
+                  sample text
+                </p>
+                <p className="text-time">
+                  00/00/00:0
+                </p>
+              </div>
+            </div>
+
+            <div className="agenda-wrap">
+              <div className="agenda-left">
+                <div className="agenda-title">
+                  <p>sample agenda</p>
+                </div>
+                <div className="agenda-totalling">
+                  <p>賛成:1 反対:2 計:3</p>
+                </div>
+              </div>
+              <div className="agenda-post">
+                <p className="agenda-text">
+                  sample text
+                </p>
+                <p className="text-time">
+                  00/00/00:0
+                </p>
+              </div>
+            </div>
+
+            <div className="agenda-wrap">
+              <div className="agenda-left">
+                <div className="agenda-title">
+                  <p>sample agenda</p>
+                </div>
+                <div className="agenda-totalling">
+                  <p>賛成:1 反対:2 計:3</p>
+                </div>
+              </div>
+              <div className="agenda-post">
+                <p className="agenda-text">
+                  sample text
+                </p>
+                <p className="text-time">
+                  00/00/00:0
+                </p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TopPage
