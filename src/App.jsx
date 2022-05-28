f7b672d9998b0f07a697e1903bfcb5721cd03279
saitohiroki <hirokisaito0813@gmail.com>
2022-05-28 13:34:00 +0900

    参考

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,40 @@
-import React from 'react';
-// import errorData from './error';
-import './assets/styles/login.css';
-// import LoginFormBox from './components/Login/LoginFormBox';
-import CreateUser from './components/Register/CreateUser';
+import React, { useState, useEffect } from 'react';
+import TopPage from './components/Top/TopPage';
+// import CleanUP from './components/Top/CleanUP';
+import styles from "./components/css/destyle.css";
 
-// export default class App extends React.Component {
-//   constructor(props){
-//     super(props);
-//     this.state = {
-//       id:[],
-//       pass:[],
-//       myid:"myid",
-//       mypass:"mypass",
-//       error:errorData,
-//       form:"xxx"
-//     }
-//     // this.gologin = this.gologin.bind(this)
-//   }
-
-  // loginBool = () => {
-  //   const loginbool = this.state.form;
-
-  //   this.setState({
-  //     initForm
-  //   })
-  // }
-  // componentDidMount() {
-  //   this.initForm()
-  // }
-
-  // initError = () => {
-  //   const initErrorSet = this.state.error[this.state.errorData];
-  //   //const initError = "initerror";
-  //   this.setState({
-  //     error:initErrorSet
-  //   })
-  // }
-  // componentDidMount() {
-  //   this.initError()
-  // }
-
-//   render (){
-//     return (
-//       <div className='login-pare'>
-//         <div className='login-main-box'>
-//           <LoginFormBox errors={this.state.error} isError={this.state.myid} gologin={this.gologin} />
-//         </div>
-//       </div>
-//     );
-//  }
-// render(){
-
-// }
-// }
 function App() {
+  // const [status, setStatus] = useState("test");
+  // const [input, setInput] = useState("");
+  // const [counter, setCounter] = useState(0);
+  // const [display, setDisplay] = useState(true);
+
+  // const onChangeHandler = (e) => {
+  //   setInput(e.target.value)
+  // }
+
+  useEffect(() => {
+    // console.log("useEffect in App invoked !");
+    // document.title = `current value is ${counter}`;
+  }, []);
+
   return (
-    <CreateUser />
+    // <div className="App">
+    //   <h1 className="test">{status}</h1>
+    //   <button onClick={() => setStatus("new text")}>new textに変換</button>
+    //   <h2 className="input">{input}</h2>
+    //   <input type="text" value={input} onChange={onChangeHandler} />
+    //   <h3 className="counter">{counter}</h3>
+    //   <button onClick={() => setCounter((preCounter) => preCounter + 1)}>+</button>
+    //   {display && <CleanUP/>}
+    //   <button onClick={() => setDisplay(!display)}>displayスイッチ</button>
+    //   {console.log(display)}
+    // </div>
+    <div className="App">
+      <div className="header">ヘッダー</div>
+      <TopPage />
+    </div>
+
   )
 }
 
