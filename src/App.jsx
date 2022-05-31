acd084bc68622c9bbaf97e2ab706bc753629e647
chika83 <chikashige.8.3@gmail.com>
2022-05-31 22:05:29 +0900

    app edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import React, { useState, useEffect } from 'react';
-// import TopPage from './components/Top/TopPage';
+//import React, { useState, useEffect } from 'react';
+import TopPage from './components/Top/TopPage';
 import SignIn from './components/Register/Register';
 //import styles from "./components/css/destyle.css";
 import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login/Login';
+import HeaderBox from './components/Header/Header';
 
 function App() {
 
   return (
     <AuthProvider>
-    <div className="App">
-      <div className="header">ヘッダー</div>
-      {/* <TopPage /> */}
-      <SignIn />
-    </div>
+
+      <div className="App">
+        <BrowserRouter>
+        <HeaderBox />
+          <Routes>
+            <Route exact path="/" element={<TopPage />} />
+            <Route path="/register" element={<SignIn />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </AuthProvider>
   )
 }
